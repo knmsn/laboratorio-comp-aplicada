a763fcf797517ffaaf000880be7c1507a14da48e
Gustavo de Oliveira Rosa <gustavoolrosa2019@gmail.com>
2024-04-22 19:48:17 -0300

    update: Update login validate and datasource

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const availableUsers = [
+  { email: "admin", password: "admin" },
+  { email: "user1", password: "password1" },
+];
+
 export default function Login() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    if (email === "admin" && password === "admin") {
-      AsyncStorage.setItem('logged', 'true');
-      navigate("/home");
-    } else {
-      alert("DADOS INCORRETOS, TENTE NOVAMENTE");
+  const handleLogin = async () => {
+    if (email === "" || password === "") {
+      alert("Usuário ou senha em branco, verifique.");
+      return;
+    }
+    const matchedUser = availableUsers.find(user => user.email === email && user.password === password);
+    if (matchedUser) {
+      try {
+        await AsyncStorage.setItem('logged', 'true');
+        navigate("/home");
+      } catch (error) {
+        console.error('Erro fazer validacao de login:', error);
       }
+    } else {
+      alert("Usuário ou senha incorretos.");
+    }
   };
+  
 
   return (
     <>
